e3ea3063665f48908589c87f225c069cc7a02523
nicad <36125594+PBnicad@users.noreply.github.com>
2023-10-31 18:20:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import asyncio
 import os
 
 from bing.wallpaper import explore_wallpaper
-from pixiv.ranking import weekly_ranking
+#from pixiv.ranking import weekly_ranking
 from telegram import Bot
-from yamibo.manga import yuri_manga
+#from yamibo.manga import yuri_manga
 
 TOKEN = os.environ.get("TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
@@ -22,6 +22,7 @@ bot = Bot(token=TOKEN)
 
 
 async def main():
+   '''
     # Bing 壁纸
     image, info = explore_wallpaper()
     await bot.sendPhoto(
@@ -29,7 +30,7 @@ async def main():
         photo=image,
         caption=info
     )
-'''
+
     # Pixiv 插画周榜
     lst = weekly_ranking()
     await bot.sendMediaGroup(
